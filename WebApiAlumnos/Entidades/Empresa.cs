b1829214666b57c96b28fa29719b057464d64f23
Alessandro Elizondo <alessandro.uanl@gmail.com>
2022-09-19 16:40:59 -0500

    Algunas validaciones
    Validaciones Required y Range

--- WebApiAlumnos/Entidades/Empresa.cs
+++ WebApiAlumnos/Entidades/Empresa.cs
@@ -1,10 +1,18 @@
-﻿namespace WebApiAlumnos.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApiAlumnos.Entidades
 {
     public class Empresa
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Nombre { get; set; }
+
+        [Range(0,2022, ErrorMessage = "Año no valido")]
         public int fundacion { get; set; }
+
 
         public int PaisId { get; set; }
     }
--- WebApiAlumnos/Entidades/Pais.cs
+++ WebApiAlumnos/Entidades/Pais.cs
@@ -1,8 +1,12 @@
-﻿namespace WebApiAlumnos.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiAlumnos.Entidades
 {
     public class Pais
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Nombre { get; set; }
         public int Habitantes { get; set; }
         public List<Empresa> empresas { get; set; }
